738ed1cdf3e69990230164e40a4c68fad614ae5f
Erick Kawauche <kawauche2@gmail.com>
2016-11-09 23:38:20 -0200

    removido templates do header

--- assets/js/headerTemplate.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$("#template").load("/templates/headerTemplate.html #templateHome",function(){
-    	var template = document.getElementById('template').innerHTML;
-        var output = Mustache.render(template);
-        $("#template").html(output);
-	});
-});
